7ab163f35a3a684191252c1f7cd25131f72940f7
Finn-Ole Carstens <f.carstens@hzdr.de>
2025-06-16 15:12:22 +0200

    bug fix aperture softening? v2

--- src/picongpuanalysis/postprocessing/shadowgraphy.py
+++ src/picongpuanalysis/postprocessing/shadowgraphy.py
@@ -120,9 +120,10 @@ def apply_numerical_aperture(
 
         kxm, kym, omegam = np.meshgrid(kx, ky, omega, indexing="ij")
         k_perp = np.sqrt(kxm**2 + kym**2)
-        k_aperture = np.abs(numerical_aperture * omegam / const.c)
-        k_soft = np.abs(aperture_softening * omegam / const.c)
-        k_aperture_soft = k_aperture + k_soft
+        # k_aperture = np.abs(numerical_aperture * omegam / const.c)
+        # k_soft = np.abs(aperture_softening * omegam / const.c)
+        # k_aperture_soft = k_aperture + k_soft
+        k_aperture_soft = np.abs((numerical_aperture + aperture_softening) * omegam / const.c)
 
         if window_function is not None:
             # For each omega slice, apply the window function radially in k_perp
